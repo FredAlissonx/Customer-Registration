737b348c1fd14c4ec0682fa63b87f16fe5db871e
Fred Alisson <fredalissonsousa@gmail.com>
2023-10-26 23:18:14 -0300

    feat: iPhoneNumber implemented, version 1.0

--- src/main/java/br/com/fred/auth/ICustomerValidation.java
+++ src/main/java/br/com/fred/auth/ICustomerValidation.java
@@ -1,9 +1,8 @@
 package br.com.fred.auth;
 
-import br.com.caelum.stella.ValidationMessage;
 import br.com.caelum.stella.validation.CPFValidator;
 
-import java.util.List;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -40,6 +39,29 @@ public interface ICustomerValidation {
         // verify if phoneNumber begin with 9
         if (phoneNumber.length() == 11 && Integer.parseInt(phoneNumber.substring(2,3)) != 9) return false;
 
-        return false;
+        // verify if all character in phoneNumber are the same
+        Pattern pattern = Pattern.compile(phoneNumber.charAt(0) + "{"+phoneNumber.length()+"}");
+
+        Matcher matcher = pattern.matcher(phoneNumber);
+        if (matcher.find()) return false;
+
+        // valid DDD
+        Integer[] dddCode = {
+                11, 12, 13, 14, 15, 16, 17, 18, 19,
+                21, 22, 24, 27, 28, 31, 32, 33, 34,
+                35, 37, 38, 41, 42, 43, 44, 45, 46,
+                47, 48, 49, 51, 53, 54, 55, 61, 62,
+                64, 63, 65, 66, 67, 68, 69, 71, 73,
+                74, 75, 77, 79, 81, 82, 83, 84, 85,
+                86, 87, 88, 89, 91, 92, 93, 94, 95,
+                96, 97, 98, 99
+        };
+        // verify if contains phoneNumber DDD
+        if (!Arrays.asList(dddCode).contains(Integer.parseInt(phoneNumber.substring(0, 2)))) return false;
+
+        //if the number has length 10, it's not a smartphone number that why after DDD needs to be 2, 3, 4, 5, or 7
+        Integer[] prefix = {2, 3, 4, 5, 7};
+        // after all validations right, return true
+        return phoneNumber.length() == 10 && Arrays.asList(prefix).contains(Integer.parseInt(phoneNumber.substring(0, 2)));
     }
 }

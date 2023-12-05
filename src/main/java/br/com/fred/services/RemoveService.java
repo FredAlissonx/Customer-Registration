dd9dc7aee0044d53e00105ec2871c910f9290da9
Fred Alisson <fredalissonsousa@gmail.com>
2023-12-04 22:55:43 -0300

    feat: class to remove customers

--- src/main/java/br/com/fred/services/RemoveService.java
+++ src/main/java/br/com/fred/services/RemoveService.java
@@ -9,9 +9,7 @@ public class RemoveService {
     public static void remove(ICustomerDAO iCustomerDAO, String cpf) {
         CustomerDTO customerDTO = iCustomerDAO.consult(cpf);
         if (customerDTO != null) {
-
             iCustomerDAO.remove(cpf);
-
             JOptionPane.showMessageDialog(null, customerDTO.getFirstName() + " removed with success", "removed", JOptionPane.INFORMATION_MESSAGE);
         } else {
             JOptionPane.showMessageDialog(null, cpf + " does not exist in our register", "Notremoved", JOptionPane.INFORMATION_MESSAGE);
